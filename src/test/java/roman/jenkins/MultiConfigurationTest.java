43fa72ef01e088bf719d3d3c240077b122b2e943
Roman Burlaka <101947109+RomanBurlaka78@users.noreply.github.com>
2024-06-16 21:58:35 +0200

    Test disable, enable project multiconfiguration (#7)
    * multiconfiguration project
    
    * disable, enable project multiconfiguration

--- src/test/java/roman/jenkins/MultiConfigurationTest.java
+++ src/test/java/roman/jenkins/MultiConfigurationTest.java
@@ -5,6 +5,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import roman.jenkins.model.HomePage;
 import roman.jenkins.model.jobs.configs.MultiConfigurationConfigurePage;
+import roman.jenkins.model.jobs.details.MultiConfigurationDetailsPage;
 import roman.jenkins.runner.BaseTest;
 
 
@@ -15,7 +16,6 @@ public class MultiConfigurationTest extends BaseTest {
 
     @Test
     public void testCreate() {
-
         Boolean createProject = new HomePage(getDriver())
                 .clickNewItem()
                 .typeItemName(NAME_OF_PROJECT)
@@ -25,19 +25,57 @@ public class MultiConfigurationTest extends BaseTest {
                 .isProjectExist(NAME_OF_PROJECT);
 
         Assert.assertTrue(createProject, NAME_OF_PROJECT);
-
     }
 
     @Test(dependsOnMethods = "testCreate")
     public void testCreateWithSameName() {
-
         String createWithSameName = new HomePage(getDriver())
                 .clickNewItem()
                 .typeItemName(NAME_OF_PROJECT)
                 .getInvalidNameErrorMessage();
 
         Assert.assertEquals(createWithSameName, "» A job already exists with the name ‘" + NAME_OF_PROJECT + "’");
+    }
 
+    @Test(dependsOnMethods = "testCreate")
+    public void testFindJob() {
+        String searchProject = new HomePage(getDriver())
+                .goSearchBox(NAME_OF_PROJECT, new MultiConfigurationConfigurePage(getDriver()))
+                .getHeadLineText();
+
+        Assert.assertEquals(searchProject, "Project " + NAME_OF_PROJECT);
+    }
+
+    @Test
+    public void testCreateWithoutName() {
+        String getErrorName = new HomePage(getDriver())
+                .clickNewItem()
+                .selectMultiConfigurationProject()
+                .getRequiredNameErrorMessage();
+
+        Assert.assertEquals(getErrorName, "» This field cannot be empty, please enter a valid name");
+    }
+
+    @Test(dependsOnMethods = "testCreate")
+    public void testDisableProject() {
+        String disableProject = new HomePage(getDriver())
+                .clickJobByName(NAME_OF_PROJECT, new MultiConfigurationDetailsPage(getDriver()))
+                .clickDisableButton()
+                .getDisabledMessageText();
+
+        Assert.assertEquals(disableProject,
+                "This project is currently disabled");
+    }
+
+    @Test(dependsOnMethods = {"testCreate", "testDisableProject"})
+    public void testEnableProject() {
+        String enableProject = new HomePage(getDriver())
+                .clickJobByName(NAME_OF_PROJECT, new MultiConfigurationDetailsPage(getDriver()))
+                .clickEnableButton()
+                .getDisableEnableButtonText();
+
+        Assert.assertEquals(enableProject,
+                "Disable Project");
     }
 
 }
